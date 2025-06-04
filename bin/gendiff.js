bf35282bfdbfc091241ad5a4ab8cbacf76139dcf
ann-sed <a.sedeeva@yandex.ru>
2025-06-04 13:13:44 +0300

    Fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import gendiff from '../src/index.js';
 const program = new Command();
 
 program
@@ -7,5 +8,9 @@ program
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    //вызов функции парсинга - вся работа с файлами должна происходить в отдельном модуле.
+    gendiff(filepath1, filepath2);
+  })
 
 program.parse();
