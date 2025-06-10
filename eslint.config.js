594ccfd5a6d08e947addde20c386c7a9c1fd46e0
ann-sed <a.sedeeva@yandex.ru>
2025-06-10 23:35:21 +0300

    Add rules  eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,9 @@ import stylistic from '@stylistic/eslint-plugin';
 
 
 export default defineConfig([
+  {
+    ignores: ["eslint.config.js"]
+  },
   stylistic.configs.recommended,
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
