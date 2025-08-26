057d9d908f81fc44a5e894f90a92c9a4993a26ff
ann-sed <a.sedeeva@yandex.ru>
2025-08-26 23:32:35 +0300

    Update for diference format (YML) to src/parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,3 +1,10 @@
-export default (data) => {
-  return JSON.parse(data)
+import yaml from 'js-yaml'
+
+export default (data, fileType) => {
+  if (fileType === '.json') {
+    return JSON.parse(data)
+  }
+  else if (fileType === '.yml' || fileType === '.yaml') {
+    return yaml.load(data)
+  }
 }
