9b97f097d380b900f660575da3c3eb467b7506c0
ann-sed <a.sedeeva@yandex.ru>
2025-09-02 01:35:59 +0300

    Update function src/index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,46 @@
 import path from 'path'
 import fs from 'fs'
 import parse from './parser.js'
+import getDifference from './treeBuilder.js'
 import { cwd } from 'process'
 import _ from 'lodash'
 
-const readFile = (filename) => {
+// const getDifference = (data1, data2) => {
+//   const keys1 = Object.keys(data1)
+//   const keys2 = Object.keys(data2)
+//   const keys = _.sortBy(_.union(keys1, keys2))
+
+//   const difference = keys.reduce((acc, key) => {
+//     if (!(key in data1)) {
+//       acc += '\n' + `  + ${key}: ${data2[key]}`
+//     }
+//     else if (!(key in data2)) {
+//       acc += '\n' + `  - ${key}: ${data1[key]}`
+//     }
+//     else if (data1[key] === data2[key]) {
+//       acc += '\n' + `    ${key}: ${data1[key]}`
+//     }
+//     else {
+//       acc += '\n' + `  - ${key}: ${data1[key]}`
+//       acc += '\n' + `  + ${key}: ${data2[key]}`
+//     }
+//     return acc
+//   }, '')
+//   const result = '{' + difference + '\n}'
+//   return result
+// }
+
+const getData = (filename) => {
   const filepath = path.resolve(cwd(), filename)
-  return fs.readFileSync(filepath, 'utf-8')
+  const rowData = fs.readFileSync(filepath, 'utf-8')
+  const format = path.extname(filename)
+  const data = parse(rowData, format)
+  return data
 }
 
-const getDifference = (data1, data2) => {
-  const keys1 = Object.keys(data1)
-  const keys2 = Object.keys(data2)
-  const keys = _.sortBy(_.union(keys1, keys2))
-
-  const difference = keys.reduce((acc, key) => {
-    if (!(key in data1)) {
-      acc += '\n' + `  + ${key}: ${data2[key]}`
-    }
-    else if (!(key in data2)) {
-      acc += '\n' + `  - ${key}: ${data1[key]}`
-    }
-    else if (data1[key] === data2[key]) {
-      acc += '\n' + `    ${key}: ${data1[key]}`
-    }
-    else {
-      acc += '\n' + `  - ${key}: ${data1[key]}`
-      acc += '\n' + `  + ${key}: ${data2[key]}`
-    }
-    return acc
-  }, '')
-  const result = '{' + difference + '\n}'
-  return result
+const genDiff = (filename1, filename2) => { // тут нужно задать формат по умолчанию
+  const data1 = getData(filename1)
+  const data2 = getData(filename2)
+  return getDifference(data1, data2) 
 }
-
-export default (filename1, filename2) => {
-  const rowData1 = readFile(filename1)
-  const rowData2 = readFile(filename2)
-
-  const fileType1 = path.extname(filename1)
-  const fileType2 = path.extname(filename2)
-
-  const data1 = parse(rowData1, fileType1)
-  const data2 = parse(rowData2, fileType2)
-  return getDifference(data1, data2)
-}
+export default genDiff
