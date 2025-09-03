e1fe2a2f0b2c2f7f96eb960a1218d6ab2209f1c0
ann-sed <a.sedeeva@yandex.ru>
2025-09-03 15:41:50 +0300

    Fix src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,33 +1,33 @@
+import _ from 'lodash'
+
 const specialSymbol = {
-    added: '+.',
-    removed: '-.',
+    added: '+ ',
+    removed: '- ',
     equal: '  ',
-    modified: '  ',
+    changed: '  ',
+    nested: '  ',
 }
 
-const iter = (value,depth = 1, indentionCount = 4, replacer = '.') => {
-  const indentSize =  depth * indentionCount 
-  const currentIndent = replacer.repeat(indentSize - 2)
-  const bracketIndent = replacer.repeat(indentSize - indentionCount)
+const replacer = ' '
+const indentionCount = 4
 
-  const lines = value.map(node => {
-    const { name, type, status, children, value } = node
-    const specSymbol = specialSymbol[status]
-    
-    if (type === 'plain') {
-      return `${currentIndent}${specSymbol}${name}: ${value}`
+const stringify = (data, depth) => {
+  if (!_.isPlainObject(data)) {
+    return data
+  }
+
+  const indentSize =  depth * indentionCount 
+  const indent = replacer.repeat(indentSize)
+  const bracketIndent = replacer.repeat(indentSize - indentionCount) // -4 для скобок, так как они находятся на -1 вложенности
+
+  const lines = Object.keys(data).map((key) => {
+    const value = data[key]
+
+    if (_.isPlainObject(value)) {
+      return `${indent}${key}: ${stringify(value, depth + 1)}`
     }
-    else if (type === 'nested' && status === 'modified') {
-      return `${currentIndent}${specSymbol}${name}: ${iter(children, depth + 1)}`
-    }
-    else {
-      return [
-        // `${currentIndent}${specSymbol}${name}: {`,
-        iter(children, depth + 1),
-        `${bracketIndent}}`
-        ].join('\n')
-    }
-    })
+    return `${indent}${key}: ${value}`
+  })
   return [
     '{',
     ...lines,
@@ -35,4 +35,33 @@ const iter = (value,depth = 1, indentionCount = 4, replacer = '.') => {
   ].join('\n')
 }
 
-export default (dataTree) => iter(dataTree)
+const stilish = (nodes) => {
+
+  const iter = (node, depth = 1) => {
+    const indentSize = depth * indentionCount
+    const indent = replacer.repeat(indentSize - 2) // -2 для спецсимволов
+    const bracketIndent = replacer.repeat(indentSize) //  для скобок, так как они находятся на +1 вложенности
+
+    const { name, type, value, value1, value2, children } = node
+    const symbol = specialSymbol[type]
+
+    if (type === 'changed') {
+      const plus = specialSymbol.added
+      const minus = specialSymbol.removed
+      const removedValue = stringify(value1, depth + 1)
+      const addedValue = stringify(value2, depth + 1)
+
+      return `\n${indent}${minus}${name}: ${removedValue}\n${indent}${plus}${name}: ${addedValue}`
+    }
+    if (type === 'nested') {
+      const nestedValue = children.map((child) => iter(child, depth + 1)).join('')
+      return `\n${indent}${symbol}${name}: {${nestedValue}\n${bracketIndent}}`;
+    }
+    return `\n${indent}${symbol}${name}: ${stringify(value, depth + 1)}`
+  }
+  
+  const lines = nodes.map(node => iter(node))
+  return `{${lines.join('')}\n}`
+}
+export default stilish
+
